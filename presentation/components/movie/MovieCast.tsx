deab3c4acf4097da3df7e0dba77ce5c8fd74ecfb
Chinazo <chinazoonwukaike@gmail.com>
2025-03-24 16:25:59 -0500

    change map to FlatList

--- presentation/components/movie/MovieCast.tsx
+++ presentation/components/movie/MovieCast.tsx
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import useGetMovieCast from "../../../presentation/hooks/useGetMovieCast";
 import ActorCard from "./ActorCard";
+import { FlatList } from "react-native-gesture-handler";
 
 type MovieCastType = {
   id: string;
@@ -9,10 +10,15 @@ const MovieCast = ({ id }: MovieCastType) => {
   const { cast } = useGetMovieCast(String(id));
 
   return (
-    <View className="flex flex-row">
-      {cast?.map((actor) => (
-        <ActorCard actor={actor} />
-      ))}
+    <View className="mt-5 mb-20">
+      <Text className="font-bold text-2xl px-5">Actors</Text>
+      <FlatList
+        data={cast}
+        keyExtractor={(item) => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <ActorCard actor={item} />}
+      />
     </View>
   );
 };
